b33727db93215260c12a8641be497367c3b6e0fd
TamaraDimov <tamemojsoska@gmail.com>
2023-02-22 17:26:31 +0100

    implement funcionality to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,82 @@
+import React, { useState } from 'react';
+import calculate from './logic/calculate';
 import './Calculator.css';
 
-import React from 'react';
-
 function Calculator() {
+  const [results, setRes] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    setRes(calculate(results, e.target.textContent));
+  };
+
   return (
     <div className="container">
-      <div className="screen" />
-      <div className="buttons no">AC</div>
-      <div className="buttons no">+/-</div>
-      <div className="buttons no">%</div>
-      <div className="buttons func">÷</div>
-      <div className="buttons no">7</div>
-      <div className="buttons no">8</div>
-      <div className="buttons no">9</div>
-      <div className="buttons func">*</div>
-      <div className="buttons no">4</div>
-      <div className="buttons no">5</div>
-      <div className="buttons no">6</div>
-      <div className="buttons func">+</div>
-      <div className="buttons no">1</div>
-      <div className="buttons no">2</div>
-      <div className="buttons no">3</div>
-      <div className="buttons func">-</div>
-      <div className="buttons zero">0</div>
-      <div className="buttons">.</div>
-      <div className="buttons func">=</div>
+      <input
+        className="screen"
+        type="text"
+        value={results.next || results.total || '0'}
+      />
+      <button type="button" className="buttons no" onClick={handleClick}>
+        AC
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        +/-
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        %
+      </button>
+      <button type="button" className="buttons func" onClick={handleClick}>
+        ÷
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        7
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        8
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        9
+      </button>
+      <button type="button" className="buttons func" onClick={handleClick}>
+        *
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        4
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        5
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        6
+      </button>
+      <button type="button" className="buttons func" onClick={handleClick}>
+        +
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        1
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        2
+      </button>
+      <button type="button" className="buttons no" onClick={handleClick}>
+        3
+      </button>
+      <button type="button" className="buttons func" onClick={handleClick}>
+        -
+      </button>
+      <button type="button" className="buttons zero" onClick={handleClick}>
+        0
+      </button>
+      <button type="button" className="buttons" onClick={handleClick}>
+        .
+      </button>
+      <button type="button" className="buttons func" onClick={handleClick}>
+        =
+      </button>
     </div>
   );
 }
