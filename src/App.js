90961201e0e5176b910d4dce897d8a5f58a2d5ba
TamaraDimov <tamemojsoska@gmail.com>
2023-02-28 14:09:49 +0100

    revert commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
-import Nav from './components/Nav';
-import Quote from './components/Quote';
-import Home from './components/Home';
 
-const App = () => (
-  <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Nav />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/quote" element={<Quote />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
 
 export default App;
