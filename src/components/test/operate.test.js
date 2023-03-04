04bcf770ffc505ba75e7e9144a0ce7228652d059
Ernest Korir <ernestbore3@gmail.com>
2023-03-04 17:29:32 +0300

    Add Operate test

--- src/components/test/operate.test.js
+++ src/components/test/operate.test.js
@@ -12,4 +12,7 @@ describe('operate', () => {
   test('multiplies two numbers', () => {
     expect(operate('4', '3', '*')).toBe('12');
   });
+  test('expect 10 ÷ 0 to be "It is not divisable by 0."', () => {
+    expect(operate(1, 0, '÷')).toBe("Can't divide by 0.");
+  });
 });
